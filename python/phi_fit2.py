5b967c61c7e9c7565825ef1f2d69a54aa1a69a1e
Kim Jalosjos <jalosjos-kim@ku.edu>
2022-03-09 16:16:02 -0600

    output rounded values and errors, added cut

--- python/phi_fit2.py
+++ python/phi_fit2.py
@@ -1,13 +1,13 @@
 #phi_fit2.py
 
 import ROOT
-import os
 import iminuit
+import csv
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 from iminuit import Minuit
 from iminuit.cost import LeastSquares
-import string
 from matplotlib.backends.backend_pdf import PdfPages
 
 def makeDir(dir_name):
@@ -20,26 +20,18 @@ ROOT.gROOT.SetBatch(ROOT.kTRUE)
 # Tell ROOT not to be in charge of memory, fix issue of histograms being deleted when ROOT file is closed:
 ROOT.TH1.AddDirectory(False)
 
-def fit(input_file, input_dir, plot_dir, y_min, y_max):
+def fit(input_file, input_dir, plot_dir):
 
     print("Fitting for input file {0}".format(input_file))
 
     makeDir(plot_dir)
 
-    pi = 3.14159265
-
     g = []
     c = []    
     
     a = ROOT.TFile("{0}/{1}.root".format(input_dir, input_file), "READ")
-    #f = ROOT.TF1("f", "[0]*sin(x - [1]) + [2]", -pi, pi)
-
-    #f.SetParNames("amp", "shift", "offset")
-    #f.SetParLimits(0, 0, 1e10)
-    #f.SetParLimits(1, -pi, pi)
 
     tag = "postcut"
-
     base_name = plot_dir + "/" + input_file + "_" + tag
 
     num_phi =  a.Get("gr_num_phi_per_ring")
@@ -48,95 +40,106 @@ def fit(input_file, input_dir, plot_dir, y_min, y_max):
     else:          
         print("FAIL: did not load num_phi")
 
-    pp = PdfPages("{0}2.pdf".format(input_file))
+    pp = PdfPages("{0}_cut.pdf".format(input_file))
 
-    for i in range(64):
+    csv_output_name = "{0}_cut_parameters.csv".format(input_file)
+    output_column_titles = ["ring", "nop", "amp", "amp error", "shift", "shift error", "offset", "offset error"]
+    with open(csv_output_name, 'w', newline='') as output_csv:
+        output_writer = csv.writer(output_csv)
+        output_writer.writerow(output_column_titles)
 
-        #f.SetParameters(4386, 1.484, 1.132e5)
+        for i in range(64):
 
-        ring_name = "gr_phi_occ_ring_{0}_{1}".format(tag, i)
-        file_ring_name = "{0} gr_phi_occ_ring_{1}_{2}".format(input_file, tag, i)
-        
-        g.append (a.Get(ring_name))
-        c.append (ROOT.TCanvas(file_ring_name, file_ring_name))
+            ring_name = "gr_phi_occ_ring_{0}_{1}".format(tag, i)
+            file_ring_name = "{0} gr_phi_occ_ring_{1}_{2}".format(input_file, tag, i)
 
-        n = g[-1].GetN()
-        data_x = []
-        data_y = []
-        data_yerr = []
-        
-        for j in range(n):
-            data_x.append(g[-1].GetPointX(j))
-            data_y.append(g[-1].GetPointY(j))
-            data_yerr.append(np.sqrt(g[-1].GetPointY(j)))
+            g.append (a.Get(ring_name))
+            c.append (ROOT.TCanvas(file_ring_name, file_ring_name))
 
-        min_y = np.min(data_y)
-        max_y = np.max(data_y)
-        delta_y = max_y - min_y
-        #h_y = 0.8*delta_y + min_y
-        h_y = 250000
-        c_start = np.mean(data_y)
-        least_squares = LeastSquares(data_x, data_y, data_yerr, sin)
-        m = Minuit(least_squares, a=5000, b=0, c=c_start, limit_a=[0,20000], limit_b=[-pi,pi])
-        #m = Minuit(least_squares, a=0, b=0)
+            n = g[-1].GetN()
 
-        m.migrad() 
-        m.hesse()
-        print(m.values)
-        #print(m.values["error_b"])
-        print(m.get_param_states())
-        print(m.get_fmin())
-        
-        #print(m.values["a"])
-        a_fit = m.values["a"]
-        b_fit = m.values["b"]
-        c_fit = m.values["c"]
- 
-        a_fit_list = np.array(n*[a_fit])
-        b_fit_list = np.array(n*[b_fit])
-        c_fit_list = np.array(n*[c_fit])
+            # cut on number of points
+            if n < 9:
+                continue
 
-        xf = np.arange(-pi, pi, 0.1)
-        nf = len(xf)
-        an = np.array(nf*[a_fit])
-        bn = np.array(nf*[b_fit])
-        cn = np.array(nf*[c_fit])
+            data_x = []
+            data_y = []
+            data_yerr = []
 
-        yf = sin(xf, an, bn, cn)
+            for j in range(n):
+                data_x.append(g[-1].GetPointX(j))
+                data_y.append(g[-1].GetPointY(j))
+                data_yerr.append(np.sqrt(g[-1].GetPointY(j)))
 
-        plt.errorbar(data_x, data_y, data_yerr, fmt="o", label="data")
-        plt.plot(xf, yf, label="fit")
-        #plt.plot(xf, sin(xf, a_fit_list, b_fit_list, c_fit_list), label="fit")
-        plt.text(-2, h_y, "a={0:.2f}\nb={1:.2f}\nc={2:.2f}".format(a_fit, b_fit, c_fit))
-        plt.legend()
-        plt.title("Ring {0}".format(i))
-        plt.xlabel("phi")
-        plt.ylabel("occupancy")
-        plt.xlim([-pi, pi])
-        plt.ylim([0, 300000])
-        #plt.savefig("plots/ring{0}".format(i))
-        pp.savefig()
-        plt.clf()
-        #print("n={0}".format(n))
-        #print("x={0}".format(data_x))
-        #print("y={0}".format(data_y))
-        #g[-1].Fit(f, "R")
-        #g[-1].SetTitle(ring_name)
-        
-        #g[-1].SetMarkerStyle(20)
-        #g[-1].GetYaxis().SetRangeUser(y_min, y_max)
-   
-        #c[-1].cd()
-        #g[-1].Draw("AP")
-        
+            min_y = np.min(data_y)
+            max_y = np.max(data_y)
+            delta_y = max_y - min_y
+            h_y = 250000
+            c_start = np.mean(data_y)
+
+            least_squares = LeastSquares(data_x, data_y, data_yerr, sin)
+            m = Minuit(least_squares, a=5000, b=0, c=c_start, limit_a=[0,20000], limit_b=[-np.pi,np.pi])
+            
+            m.migrad() 
+            m.hesse()
+            print(m.values)
+            print(m.errors)
+            print(m.get_param_states())
+            print(m.get_fmin())
+
+            a_fit = m.values["a"]
+            b_fit = m.values["b"]
+            c_fit = m.values["c"]
+            a_fit_errors = m.errors["a"]
+            b_fit_errors = m.errors["b"]
+            c_fit_errors = m.errors["c"]
+
+            ra_fit = float('%.2g' % m.values["a"])
+            rb_fit = float('%.2g' % m.values["b"])
+            rc_fit = float('%.2g' % m.values["c"])
+            ra_fit_errors = float('%.2g' % m.errors["a"])
+            rb_fit_errors = float('%.2g' % m.errors["b"])
+            rc_fit_errors = float('%.2g' % m.errors["c"])
+
+            row = [i, n, ra_fit, ra_fit_errors, rb_fit, rb_fit_errors, rc_fit, rc_fit_errors]
+            output_writer.writerow(row)
+
+            a_fit_list = np.array(n*[a_fit])
+            b_fit_list = np.array(n*[b_fit])
+            c_fit_list = np.array(n*[c_fit])
+
+            xf = np.arange(-np.pi, np.pi, 0.1)
+            nf = len(xf)
+            an = np.array(nf*[a_fit])
+            bn = np.array(nf*[b_fit])
+            cn = np.array(nf*[c_fit])
+            yf = sin(xf, an, bn, cn)
+            
+            plt.errorbar(data_x, data_y, data_yerr, fmt="o", label="data")
+            plt.plot(xf, yf, label="fit")
+            #plt.plot(xf, sin(xf, a_fit_list, b_fit_list, c_fit_list), label="fit")
+            #plt.text(-2, h_y, "a={0:.2f}\nb={1:.2f}\nc={2:.2f}".format(a_fit, b_fit, c_fit))
+
+            fit_info = [f"$\\chi^2$ / $n_\\mathrm{{dof}}$ = {m.fval:.1f} / {len(data_x) - m.nfit}"]
+            fit_info.append("${0} = {1} \\pm {2}$".format("amp", ra_fit, ra_fit_errors))
+            fit_info.append("${0} = {1} \\pm {2}$".format("shift", rb_fit, rb_fit_errors))
+            fit_info.append("${0} = {1} \\pm {2}$".format("offset", rc_fit, rc_fit_errors))
+            plt.legend(title="\n".join(fit_info))
+
+            plt.title("Ring {0}".format(i))
+            plt.xlabel("phi")
+            plt.ylabel("occupancy")
+            plt.xlim([-np.pi, np.pi])
+            plt.ylim([0, 300000])
+            #plt.savefig("plots/ring{0}".format(i))
+            pp.savefig()
+            plt.clf()
+            #print("n={0}".format(n))
+            #print("x={0}".format(data_x))
+            #print("y={0}".format(data_y))
+            
         
     pp.close()
-'''        
-    c[0].Print("{0}.pdf(".format(base_name))
-    for i in range(1,63):
-        c[i].Print("{0}.pdf".format(base_name))
-    c[63].Print("{0}.pdf)".format(base_name))
-'''
 
 def sin(x, a, b, c):
     return a*np.sin(x-b)+c
@@ -148,17 +151,10 @@ def loop():
 
     input_dir = "output"
     plot_dir = "phi_fit_plots"
-    input_files = []
-    y_min_vals = []
-    y_max_vals = []
-
-    input_files.append("SingleMuon_2017B_Legacy_MoreEvents_ClusterSize2_NumberClusters2000_AllClusters")
-
-    y_min_vals.append(0.0)  
-    y_max_vals.append(300000.0)
+    input_files = ["SingleMuon_2017B_Legacy_MoreEvents_ClusterSize2_NumberClusters2000_AllClusters"]
 
     for i in range(len(input_files)):
-        fit(input_files[i], input_dir, plot_dir, y_min_vals[i], y_max_vals[i])
+        fit(input_files[i], input_dir, plot_dir)
 
 def main():
 
