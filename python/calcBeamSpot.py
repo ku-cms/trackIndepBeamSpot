9cd65de2d327f4ee79577bb3dabb93695536e537
Caleb James Smith <colorado.caleb.smith@gmail.com>
2022-03-31 10:56:19 -0500

    Calculate expected fit params for more datasets

--- python/calcBeamSpot.py
+++ python/calcBeamSpot.py
@@ -36,30 +36,86 @@ def printBeamSpot(beam_spot):
     x = beam_spot[0]
     y = beam_spot[1]
     z = beam_spot[2]
-    print("x = {0:.4f}".format(x))
-    print("y = {0:.4f}".format(y))
-    print("z = {0:.4f}".format(z))
+    print("x = {0:.2f}".format(x))
+    print("y = {0:.2f}".format(y))
+    print("z = {0:.2f}".format(z))
 
 # print parameters (b, r_2D, r_3D)
 def printParams(params):
     b    = params[0]
     r_2D = params[1]
     r_3D = params[2]
-    print("b    = {0:.4f}".format(b))
-    print("r_2D = {0:.4f}".format(r_2D))
-    print("r_3D = {0:.4f}".format(r_3D))
+    print("b    = {0:.2f}".format(b))
+    print("r_2D = {0:.2f}".format(r_2D))
+    print("r_3D = {0:.2f}".format(r_3D))
+
+def run(dataset):
+    name      = dataset["name"]
+    beam_spot = dataset["beam_spot"]
+    params    = calcParams(beam_spot)
+    print(name)
+    printBeamSpot(beam_spot)
+    printParams(params)
 
 def main():
     # 2021 express data, run 346512
-    x =  0.1727
-    y = -0.1913
-    z =  0.3813
-    beam_spot       = [x, y, z]
-    params          = calcParams(beam_spot)
+    #x =  0.1727
+    #y = -0.1913
+    #z =  0.3813
+    #beam_spot       = [x, y, z]
+    #params          = calcParams(beam_spot)
     #beam_spot_v2    = calcBeamSpot(params)
-    printBeamSpot(beam_spot)
-    printParams(params)
+    #printBeamSpot(beam_spot)
+    #printParams(params)
     #printBeamSpot(beam_spot_v2)
+
+    datasets = []
+
+    # TTBar MC (z-smeared)
+    x =  0.10
+    y = -0.08
+    z =  0.00
+    beam_spot = [x, y, z]
+    dataset = {}
+    dataset["name"] = "TTBar MC (z-smeared)"
+    dataset["beam_spot"] = beam_spot
+    datasets.append(dataset)
+
+    # Single Muon 2017B
+    x =  0.09
+    y = -0.04
+    z =  0.09
+    beam_spot = [x, y, z]
+    dataset = {}
+    dataset["name"] = "Single Muon 2017B"
+    dataset["beam_spot"] = beam_spot
+    datasets.append(dataset)
+
+    # Zero Bias 2017B
+    x =  0.09
+    y = -0.04
+    z =  0.17
+    beam_spot = [x, y, z]
+    dataset = {}
+    dataset["name"] = "Zero Bias 2017B"
+    dataset["beam_spot"] = beam_spot
+    datasets.append(dataset)
+
+    # Express Data 2021, run 346512
+    #x =  0.1727
+    #y = -0.1913
+    #z =  0.3813
+    x =  0.17
+    y = -0.19
+    z =  0.38
+    beam_spot = [x, y, z]
+    dataset = {}
+    dataset["name"] = "Express Data 2021"
+    dataset["beam_spot"] = beam_spot
+    datasets.append(dataset)
+
+    for dataset in datasets:
+        run(dataset)
 
 if __name__ == "__main__":
     main()
