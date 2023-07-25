cd5a72d3ff11c06a042909ab1f7c3c1ce69915c1
Caleb James Smith <colorado.caleb.smith@gmail.com>
2023-07-25 11:47:02 -0500

    Start new script to remove files; cannot find previous version of this script.

---
+++ python/removeFiles.py
@@ -0,0 +1,13 @@
+# removeFiles.py
+
+import time
+
+def main():
+    t_start = time.time()
+    t_stop  = time.time()
+    t_run   = t_stop - t_start
+    print("run time (sec): {0:.3f}".format(t_run))
+
+if __name__ == "__main__":
+    main()
+
